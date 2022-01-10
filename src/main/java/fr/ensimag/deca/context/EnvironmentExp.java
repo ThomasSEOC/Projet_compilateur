cbfd5915f9ee3216bf711b1106782373090ab5e3
Loïc Falgayrac <loic.falgayrac@phelma.grenoble-inp.fr>
2022-01-10 13:45:57 +0100

    EnvironnementExp

--- src/main/java/fr/ensimag/deca/context/EnvironmentExp.java
+++ src/main/java/fr/ensimag/deca/context/EnvironmentExp.java
@@ -2,6 +2,9 @@ package fr.ensimag.deca.context;
 
 import fr.ensimag.deca.tools.SymbolTable.Symbol;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Dictionary associating identifier's ExpDefinition to their names.
  * 
@@ -24,9 +27,12 @@ public class EnvironmentExp {
     // environnement (association nom -> définition, avec possibilité
     // d'empilement).
 
+    private Map<Symbol, ExpDefinition> dico = new HashMap<Symbol, ExpDefinition>();
+
     EnvironmentExp parentEnvironment; //Superclass
     
     public EnvironmentExp(EnvironmentExp parentEnvironment) {
+        //permet de conserver la hiérarchie
         this.parentEnvironment = parentEnvironment;
     }
 
@@ -39,21 +45,8 @@ public class EnvironmentExp {
      * symbol is undefined.
      */
     public ExpDefinition get(Symbol key) {
-        //renvoie Expdefinition à partir de Symbol key:
-        //key est un string
-        String name = key.getName();
-        //Pour println ("Hello world"), 2 choix possibles: println ou chaine de caract
-        //Expdefinition = expnature x Type Expdefinition(type, location)
-        //expnature = {param,var} pour hello world
-        //Un typedef est un type et un type-nature
-        //Localisation:
-        //Location location = new Location();
 
-
-
-
-
-        throw new UnsupportedOperationException("not yet implemented");
+        return (dico.get(key));
     }
 
     /**
@@ -72,7 +65,9 @@ public class EnvironmentExp {
      *
      */
     public void declare(Symbol name, ExpDefinition def) throws DoubleDefException {
-        throw new UnsupportedOperationException("not yet implemented");
+        dico.put(name, def);
+        return;
+        //throw new UnsupportedOperationException("not yet implemented");
     }
 
 }
