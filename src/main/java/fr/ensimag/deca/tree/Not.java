31ddb28ed88feb205d5d56c24ccf8007a78d8968
Jules Ho-Sun <Jules.Ho-Sun@grenoble-inp.org>
2022-01-11 18:58:45 +0100

    Implémentation de verifyExpr dans les classes Not et UnaryMinus

--- src/main/java/fr/ensimag/deca/tree/Not.java
+++ src/main/java/fr/ensimag/deca/tree/Not.java
@@ -20,12 +20,22 @@ public class Not extends AbstractUnaryExpr {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        throw new UnsupportedOperationException("not yet implemented");
+	Type typeOperand = operand.getType();
+	if (typeOperand.isBoolean()) {
+	    return typeOperand();
+	}
+	throw new ContextualError("not(" + operand + ") : " + operand + " is not a boolean");
     }
 
-
+    
     @Override
     protected String getOperatorName() {
         return "!";
     }
+}
+
+@Override
+    protected String getOperatorName() {
+    return "!";
+}
 }
--- src/main/java/fr/ensimag/deca/tree/UnaryMinus.java
+++ src/main/java/fr/ensimag/deca/tree/UnaryMinus.java
@@ -19,7 +19,11 @@ public class UnaryMinus extends AbstractUnaryExpr {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        throw new UnsupportedOperationException("not yet implemented");
+	Type typeOperand = operand.getType();
+	if (typeOperand.isInt() || typeOperand.isFloat()) {
+	    return typeOperand();
+	}
+	throw new ContextualError("not(" + operand + ") : " + operand + " is neither an int or a float");
     }
 
 
