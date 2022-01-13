572d141534d69d407eeadaef8fdab633dfba0042
Loïc Falgayrac <loic.falgayrac@phelma.grenoble-inp.fr>
2022-01-13 16:38:53 +0100

    modification fonction TestRegisterAllocator

--- src/test/java/fr/ensimag/deca/context/TestRegisterAllocator.java
+++ src/test/java/fr/ensimag/deca/context/TestRegisterAllocator.java
@@ -15,7 +15,6 @@ public class TestRegisterAllocator {
     public void testOnePhysicalRegister() {
         ContextManager contextManager = new ContextManager(null, 16);
         VirtualRegister register = contextManager.requestNewRegister();
-        System.out.println("is physical: " + register.getIsPhysicalRegister());
         DVal dval = register.getDVal();
         System.out.println("DVAL: " + dval.toString());
         GPRegister physicalRegister = register.requestPhysicalRegister();
@@ -30,26 +29,20 @@ public class TestRegisterAllocator {
         VirtualRegister register3 = contextManager.requestNewRegister();
         VirtualRegister register4 = contextManager.requestNewRegister();
 
-        System.out.println("register1 is physical: " + register1.getIsPhysicalRegister());
         DVal dval = register1.getDVal();
         System.out.println("register1 DVAL: " + dval.toString());
         GPRegister physicalRegister = register1.requestPhysicalRegister();
         System.out.println("register1 Physical register: " + physicalRegister.getNumber());
 
-        System.out.println("register2 is physical: " + register2.getIsPhysicalRegister());
         dval = register2.getDVal();
         System.out.println("register2 DVAL: " + dval.toString());
         physicalRegister = register2.requestPhysicalRegister();
         System.out.println("register2 Physical register: " + physicalRegister.getNumber());
 
-        System.out.println("register3 is physical: " + register3.getIsPhysicalRegister());
         dval = register3.getDVal();
         System.out.println("register3 DVAL: " + dval.toString());
 
-        System.out.println("register4 is physical: " + register4.getIsPhysicalRegister());
         dval = register4.getDVal();
         System.out.println("register4 DVAL: " + dval.toString());
-//        physicalRegister = register3.requestPhysicalRegister();
-//        System.out.println("register3 Physical register: " + physicalRegister.getNumber());
     }
 }
