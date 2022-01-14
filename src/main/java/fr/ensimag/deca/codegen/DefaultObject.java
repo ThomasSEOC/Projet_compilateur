677f09853f56d19dd13e03cc50d5b04fc43e34d9
Sylvain Noiry <sylvain.noiry@grenoble-inp.org>
2022-01-14 16:28:06 +0100

    méthode equal objet par défaut

--- src/main/java/fr/ensimag/deca/codegen/DefaultObject.java
+++ src/main/java/fr/ensimag/deca/codegen/DefaultObject.java
@@ -3,8 +3,7 @@ package fr.ensimag.deca.codegen;
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.tree.AbstractIdentifier;
 import fr.ensimag.ima.pseudocode.*;
-import fr.ensimag.ima.pseudocode.instructions.LOAD;
-import fr.ensimag.ima.pseudocode.instructions.STORE;
+import fr.ensimag.ima.pseudocode.instructions.*;
 
 public class DefaultObject extends AbstractClassObject {
     private final Label codeObjectEquals;
@@ -19,7 +18,14 @@ public class DefaultObject extends AbstractClassObject {
         compiler.addComment("Code for default equals method");
         compiler.addLabel(codeObjectEquals);
 
-        throw new UnsupportedOperationException("not yet implemented");
+        // first parameter is current Object address
+        // second parameter is other object address
+        compiler.addInstruction(new TSTO(2));
+        compiler.addInstruction(new BOV(compiler.getCodeGenBackend().getErrorsManager().getStackOverflowLabel()));
+        compiler.addInstruction(new LOAD(new RegisterOffset(-2, Register.LB), GPRegister.getR(0)));
+        compiler.addInstruction(new CMP(new RegisterOffset(-3, Register.LB), GPRegister.getR(0)));
+        compiler.addInstruction(new SEQ(GPRegister.getR(0)));
+        compiler.addInstruction(new RTS());
     }
 
     @Override
