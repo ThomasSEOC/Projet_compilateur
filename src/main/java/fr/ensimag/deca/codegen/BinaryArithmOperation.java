2f8ddb4f44feb8a69a9e7c66be70e77d83a72693
Sylvain Noiry <sylvain.noiry@grenoble-inp.org>
2022-01-20 22:43:55 +0100

    TOP1 Syracuse42 !

--- src/main/java/fr/ensimag/deca/codegen/BinaryArithmOperation.java
+++ src/main/java/fr/ensimag/deca/codegen/BinaryArithmOperation.java
@@ -9,6 +9,11 @@ import fr.ensimag.ima.pseudocode.ImmediateInteger;
 import fr.ensimag.ima.pseudocode.Register;
 import fr.ensimag.ima.pseudocode.instructions.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Class making binary arithmetical operations
  *
@@ -114,11 +119,42 @@ public class BinaryArithmOperation extends AbstractBinaryOperation {
 				VirtualRegister result = getCodeGenBackEnd().getContextManager().requestNewRegister(resultImm);
 				getCodeGenBackEnd().getContextManager().operationStackPush(result);
 			} else {
-				// get correct operand
-				lOp.requestPhysicalRegister();
-				getCodeGenBackEnd().addInstruction(new MUL(rOp.getDVal(), (GPRegister) lOp.getDVal()), "Operation Multiply");
-				rOp.destroy();
-				this.getCodeGenBackEnd().getContextManager().operationStackPush(lOp);
+				VirtualRegister register = null;
+				int operand = 0;
+				if (rOp.getDVal() instanceof ImmediateInteger) {
+					register = lOp;
+					operand = ((ImmediateInteger)rOp.getDVal()).getValue();
+				}
+				else if (lOp.getDVal() instanceof ImmediateInteger) {
+					register = rOp;
+					operand = ((ImmediateInteger)lOp.getDVal()).getValue();
+				}
+
+				int shiftCount = 0;
+				if (operand > 0) {
+					List<Integer> powerOfTwo = new ArrayList<>();
+					for (int i = 1; i < 9; i++) {
+						powerOfTwo.add(1 << i);
+					}
+
+					if (powerOfTwo.contains(operand)) {
+						shiftCount = powerOfTwo.indexOf(operand) + 1;
+					}
+				}
+
+				if (shiftCount > 0) {
+					for (int i = 0; i < shiftCount; i++) {
+						getCodeGenBackEnd().addInstruction(new SHL(register.requestPhysicalRegister()));
+					}
+					getCodeGenBackEnd().getContextManager().operationStackPush(register);
+				}
+				else {
+					// get correct operand
+					lOp.requestPhysicalRegister();
+					getCodeGenBackEnd().addInstruction(new MUL(rOp.getDVal(), (GPRegister) lOp.getDVal()), "Operation Multiply");
+					rOp.destroy();
+					getCodeGenBackEnd().getContextManager().operationStackPush(lOp);
+				}
 			}
 		} else if (this.getExpression() instanceof Divide) {
 
@@ -139,14 +175,38 @@ public class BinaryArithmOperation extends AbstractBinaryOperation {
 				VirtualRegister result = getCodeGenBackEnd().getContextManager().requestNewRegister(resultImm);
 				getCodeGenBackEnd().getContextManager().operationStackPush(result);
 			} else {
-				// get correct operand
-				if (lOp.getIsFloat() || rOp.getIsFloat()) {
-					getCodeGenBackEnd().addInstruction(new DIV(rOp.getDVal(), lOp.requestPhysicalRegister()), "Operation Division");
-				} else {
-					getCodeGenBackEnd().addInstruction(new QUO(rOp.getDVal(), lOp.requestPhysicalRegister()), "Operation Quotient");
+				VirtualRegister register = null;
+				int shiftCount = 0;
+				if ((rOp.getDVal() instanceof ImmediateInteger) && !lOp.getIsFloat()) {
+					register = lOp;
+					int operand = ((ImmediateInteger)rOp.getDVal()).getValue();
+					if (operand > 0) {
+						List<Integer> powerOfTwo = new ArrayList<>();
+						for (int i = 1; i < 9; i++) {
+							powerOfTwo.add(1 << i);
+						}
+						if (powerOfTwo.contains(operand)) {
+							shiftCount = powerOfTwo.indexOf(operand) + 1;
+						}
+					}
 				}
-				rOp.destroy();
-				this.getCodeGenBackEnd().getContextManager().operationStackPush(lOp);
+
+				if (shiftCount > 0) {
+					for (int i = 0; i < shiftCount; i++) {
+						getCodeGenBackEnd().addInstruction(new SHR(register.requestPhysicalRegister()));
+					}
+					getCodeGenBackEnd().getContextManager().operationStackPush(register);
+				}
+				else {
+					// get correct operand
+					if (lOp.getIsFloat() || rOp.getIsFloat()) {
+						getCodeGenBackEnd().addInstruction(new DIV(rOp.getDVal(), lOp.requestPhysicalRegister()), "Operation Division");
+					} else {
+						getCodeGenBackEnd().addInstruction(new QUO(rOp.getDVal(), lOp.requestPhysicalRegister()), "Operation Quotient");
+					}
+					rOp.destroy();
+					this.getCodeGenBackEnd().getContextManager().operationStackPush(lOp);
+				}
 			}
 		} else if (this.getExpression() instanceof Modulo) {
 
@@ -159,9 +219,37 @@ public class BinaryArithmOperation extends AbstractBinaryOperation {
 				VirtualRegister result = getCodeGenBackEnd().getContextManager().requestNewRegister(resultImm);
 				getCodeGenBackEnd().getContextManager().operationStackPush(result);
 			} else {
-				getCodeGenBackEnd().addInstruction(new REM(rOp.getDVal(), lOp.requestPhysicalRegister()), "Operation Remainder");
-				rOp.destroy();
-				this.getCodeGenBackEnd().getContextManager().operationStackPush(lOp);
+				VirtualRegister register = null;
+				int shiftCount = 0;
+				if (rOp.getDVal() instanceof ImmediateInteger) {
+					register = lOp;
+					int operand = ((ImmediateInteger)rOp.getDVal()).getValue();
+					if (operand > 0) {
+						List<Integer> powerOfTwo = new ArrayList<>();
+						for (int i = 1; i < 5; i++) {
+							powerOfTwo.add(1 << i);
+						}
+						if (powerOfTwo.contains(operand)) {
+							shiftCount = powerOfTwo.indexOf(operand) + 1;
+						}
+					}
+				}
+				if (shiftCount > 0) {
+					getCodeGenBackEnd().addInstruction(new LOAD(register.requestPhysicalRegister(), GPRegister.getR(0)));
+					for (int i = 0; i < shiftCount; i++) {
+						getCodeGenBackEnd().addInstruction(new SHR(GPRegister.getR(0)));
+					}
+					for (int i = 0; i < shiftCount; i++) {
+						getCodeGenBackEnd().addInstruction(new SHL(GPRegister.getR(0)));
+					}
+					getCodeGenBackEnd().addInstruction(new SUB(GPRegister.getR(0), register.requestPhysicalRegister()));
+					getCodeGenBackEnd().getContextManager().operationStackPush(register);
+				}
+				else {
+					getCodeGenBackEnd().addInstruction(new REM(rOp.getDVal(), lOp.requestPhysicalRegister()), "Operation Remainder");
+					rOp.destroy();
+					this.getCodeGenBackEnd().getContextManager().operationStackPush(lOp);
+				}
 			}
 		}
         else {
