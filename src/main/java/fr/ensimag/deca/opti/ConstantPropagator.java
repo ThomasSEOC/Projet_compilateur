439218cf90f57ff690d50dd64bb9fc312ef88294
Sylvain Noiry <sylvain.noiry@grenoble-inp.org>
2022-01-20 16:13:41 +0100

    début de constant propagator

---
+++ src/main/java/fr/ensimag/deca/opti/ConstantPropagator.java
@@ -0,0 +1,13 @@
+package fr.ensimag.deca.opti;
+
+public class ConstantPropagator {
+    private ControlFlowGraph graph;
+
+    public ConstantPropagator(ControlFlowGraph graph) {
+        this.graph = graph;
+    }
+
+    public void process() {
+
+    }
+}
