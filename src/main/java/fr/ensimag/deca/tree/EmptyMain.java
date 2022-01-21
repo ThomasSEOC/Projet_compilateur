1088400e5854104e1f8a2e3988d60c2e71afbc64
Sylvain Noiry <sylvain.noiry@grenoble-inp.org>
2022-01-21 10:00:52 +0100

    empty main

--- src/main/java/fr/ensimag/deca/tree/EmptyMain.java
+++ src/main/java/fr/ensimag/deca/tree/EmptyMain.java
@@ -3,6 +3,8 @@ package fr.ensimag.deca.tree;
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.ima.pseudocode.instructions.HALT;
+
 import java.io.PrintStream;
 
 /**
@@ -18,7 +20,8 @@ public class EmptyMain extends AbstractMain {
 
     @Override
     protected void codeGenMain(DecacCompiler compiler) {
-        throw new UnsupportedOperationException("not yet implemented");
+        compiler.addInstruction(new HALT());
+//        throw new UnsupportedOperationException("not yet implemented");
     }
 
     /**
