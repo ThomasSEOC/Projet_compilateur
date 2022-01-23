91b8cb2de1469a07c2ee73d913defddb83155ae0
Caroline Wang <caroline.wang@grenoble-inp.org>
2022-01-23 23:45:04 +0100

    màj pour corriger selection

--- src/main/java/fr/ensimag/deca/tree/Selection.java
+++ src/main/java/fr/ensimag/deca/tree/Selection.java
@@ -30,33 +30,37 @@ public class Selection extends AbstractLValue{
 
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass) throws ContextualError {
-	    Type selectType = expr.verifyExpr(compiler, localEnv, currentClass);
+
+        // Verify if the type exists and set it
+        Type selectType = expr.verifyExpr(compiler, localEnv, currentClass);
+        fieldIdent.verifyExpr(compiler, localEnv, currentClass);
+
         SymbolTable.Symbol exprSymbol = selectType.getName();
         ClassDefinition classDef = (ClassDefinition) compiler.getTypes().get(selectType.getName());
-	ClassType selectClass = classDef.getType();
-	FieldDefinition selectField = (FieldDefinition)classDef.getMembers().get(fieldIdent.getName());
-	Visibility visibility = selectField.getVisibility();
-	if (selectClass.isNull()) {
-	    throw new ContextualError("Null", getLocation());
-	}
-    if (classDef.isClass()){
-        throw new ContextualError(exprSymbol + " is a className, can't be used as a variable", getLocation());
-    }
-    	if (visibility == Visibility.PUBLIC) {
-	    if (selectClass != compiler.getTypes().get(selectClass.getName()).getType()) {
-		throw new ContextualError("Class not defined", getLocation());
-	    }
-	}
-	else {
-	    if (!(selectClass).isSubClassOf(currentClass.getType())) {
-		throw new ContextualError("Subtype error", getLocation());
-	    }
-	    if (!selectClass.isSubClassOf((ClassType)classDef.getType())) {
-		throw new ContextualError("Subtype error", getLocation());
-	    }
-	}
-	setType(selectField.getType());
-	return selectField.getType();
+        ClassType selectClass = classDef.getType();
+        FieldDefinition selectField = (FieldDefinition)classDef.getMembers().get(fieldIdent.getName());
+
+        if (selectClass.isNull()) {
+            throw new ContextualError("Null", getLocation());
+        }
+        if (classDef.isClass()){
+            throw new ContextualError(exprSymbol + " is a className, can't be used as a variable", getLocation());
+        }
+        if (selectField.getVisibility() == Visibility.PUBLIC) {
+            if (selectClass != compiler.getTypes().get(selectClass.getName()).getType()) {
+                throw new ContextualError("Class not defined", getLocation());
+            }
+        }
+        else {
+            if (!(selectClass).isSubClassOf(currentClass.getType())) {
+                throw new ContextualError("Subtype error", getLocation());
+            }
+            if (!selectClass.isSubClassOf(selectClass)) {
+                throw new ContextualError("Subtype error", getLocation());
+            }
+        }
+        setType(selectField.getType());
+        return selectField.getType();
     }
 
     @Override
